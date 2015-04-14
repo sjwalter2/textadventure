83752708716e3c312996f87ea8ee9cf813727d72
Roman <romnempire@gmail.com>
2015-04-14 16:41:30 -0400

    implemented screen size diffs, ren for namespace

---
+++ Assets/Scripts/PewPewDestroyByContact.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PewPewDestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	private PewPewGameController gc;
+	// Use this for initialization
+	void Start () {
+		GameObject gco = GameObject.FindWithTag ("GameController");
+		gc = gco.GetComponent <PewPewGameController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag != "Boundary") {
+
+			if(other.tag == "Player") {
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+				gc.endgame();
+			}
+			Instantiate (explosion, transform.position, transform.rotation);
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PewPewPlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PewPewPlayerController : MonoBehaviour {
+	public float speed;
+	public float xMin, xMax, zMin, zMax;
+	//public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			Debug.Log ("firing");
+			nextFire = Time.time + fireRate;
+			GameObject clone = Instantiate (shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+		}
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rigidbody.velocity = new Vector3 (moveHorizontal*speed, 0.0f, moveVertical*speed);
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp (rigidbody.position.x, xMin, xMax),
+			0f,
+			Mathf.Clamp (rigidbody.position.z, zMin, zMax)
+			);
+		rigidbody.rotation = Quaternion.Euler (0f,0f, rigidbody.velocity.x*-tilt);
+	}
+}
+
+
+//Disabled so i can edit the damn bounding box from gamecontroller for resize.
+/*
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+*/
