c447a952e3c6ac7590e9a7dd21122b749d40d991
Roman <romnempire@gmail.com>
2015-04-29 19:23:46 -0400

    bad commit gitignore issue

---
+++ Assets/Scripts/PewPewEventManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PewPewEventManager : MonoBehaviour {
+
+	public delegate void onDestroyAction(float speed);
+	public static event onDestroyAction onDestroy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(onDestroy != null) {
+//			onDestroy();
+//		}
+	}
+
+	public void boom (float speed) {
+		Debug.Log ("in boom the speed is " + speed);
+		if(onDestroy != null) {
+			onDestroy(speed);
+		}
+	}
+}
